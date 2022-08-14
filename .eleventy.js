0e0ef814bef77800e173cdef2d6511a1848fe5c2
kaelta <giorox.612nf@gmail.com>
2022-08-14 04:46:39 +0100

    add markdown-it-math functionality & rewrite societal writeup

--- .eleventy.js
+++ .eleventy.js
@@ -1,235 +1,237 @@
-const { DateTime: DT } = require('luxon');
+const {DateTime: DT} = require('luxon');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
-const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
+const {EleventyServerlessBundlerPlugin} = require('@11ty/eleventy');
 const directoryOutputPlugin = require('@11ty/eleventy-plugin-directory-output');
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const Image = require('@11ty/eleventy-img');
 const markdownIt = require('markdown-it');
 const markdownItEmoji = require('markdown-it-emoji');
 const markdownItAnchor = require('markdown-it-anchor');
+const markdownItMath = require('markdown-it-math');
 const markdownItReplaceLink = require('markdown-it-replace-link');
 const pluginTOC = require('eleventy-plugin-toc');
 
 const fs = require('fs');
 
 const handler = async event => {
-  const elev = new EleventyServerless('serverless', {
-    path: event.path, // required, the URL path
-    query: event.queryStringParameters, // optional
-  });
+	const elev = new EleventyServerless('serverless', {
+		path: event.path, // required, the URL path
+		query: event.queryStringParameters, // optional
+	});
 
-  try {
-    // Returns the HTML for the Eleventy template that matches to the URL
-    // Can use with `eleventyConfig.dataFilterSelectors` to put data cascade data into `page.data` here.
-    let [page] = await elev.getOutput();
-    let html = page.content;
+	try {
+		// Returns the HTML for the Eleventy template that matches to the URL
+		// Can use with `eleventyConfig.dataFilterSelectors` to put data cascade data into `page.data` here.
+		let [page] = await elev.getOutput();
+		let html = page.content;
 
-    return {
-      statusCode: 200,
-      body: html,
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
-    };
-  }
+		return {
+			statusCode: 200,
+			body: html,
+		};
+	} catch (err) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({error: err.message}),
+		};
+	}
 };
 
-const { builder } = require('@netlify/functions');
+const {builder} = require('@netlify/functions');
 exports.handler = builder(handler);
 
 // Create a shortcode for `sharp` optimized images.
 async function imageShortcode(
-  src,
-  alt,
-  sizes = '(min-width: 1024px) 100vw, 50vw',
+	src,
+	alt,
+	sizes = '(min-width: 1024px) 100vw, 50vw',
 ) {
-  console.log(`Generating image(s) from:  ${src}`);
-  let metadata = await Image(src, {
-    widths: [300, 600, 900],
-    formats: ['avif', 'jpeg'],
-    urlPath: '/static/img/',
-    outputPath: './_site/static/img',
-    filenameFormat: (id, src, width, format, options) => {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-      return `${name}-${width}w.${format}`;
-    },
-    sharpOptions: {
-      animated: true,
-    },
-  });
+	console.log(`Generating image(s) from:  ${src}`);
+	let metadata = await Image(src, {
+		widths: [300, 600, 900],
+		formats: ['avif', 'jpeg'],
+		urlPath: '/static/img/',
+		outputPath: './_site/static/img',
+		filenameFormat: (id, src, width, format, options) => {
+			const extension = path.extname(src);
+			const name = path.basename(src, extension);
+			return `${name}-${width}w.${format}`;
+		},
+		sharpOptions: {
+			animated: true,
+		},
+	});
 
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: 'lazy',
-    decoding: 'async',
-  };
+	let imageAttributes = {
+		alt,
+		sizes,
+		loading: 'lazy',
+		decoding: 'async',
+	};
 
-  metadata = Image.statsSync(src, options);
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes, {
-    whitespaceMode: 'inline',
-  });
+	metadata = Image.statsSync(src, options);
+	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+	return Image.generateHTML(metadata, imageAttributes, {
+		whitespaceMode: 'inline',
+	});
 }
 
 module.exports = evc => {
-  /* Add our plugins. */
-  evc.addPlugin(syntaxHighlight);
-  evc.addPlugin(pluginRss);
-  evc.addPlugin(eleventyNavigationPlugin);
-  evc.addPlugin(directoryOutputPlugin, {
-    columns: {
-      filesize: true,
-      benchmark: true,
-    },
-    warningFileSize: 400 * 1000,
-  });
-  evc.addPlugin(EleventyServerlessBundlerPlugin, {
-    name: 'art', // The serverless function name from your permalink object
-    functionsDir: './netlify/functions/',
-  });
+	/* Add our plugins. */
+	evc.addPlugin(syntaxHighlight);
+	evc.addPlugin(pluginRss);
+	evc.addPlugin(eleventyNavigationPlugin);
+	evc.addPlugin(directoryOutputPlugin, {
+		columns: {
+			filesize: true,
+			benchmark: true,
+		},
+		warningFileSize: 400 * 1000,
+	});
+	evc.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: 'art', // The serverless function name from your permalink object
+		functionsDir: './netlify/functions/',
+	});
 
-  // Generate a table of contents u.u
-  evc.addPlugin(pluginTOC);
+	// Generate a table of contents u.u
+	evc.addPlugin(pluginTOC);
 
-  /* Foambubble wiki links */
-  evc.addTransform('wiki-links', (content, outputPath) => {
-    if (outputPath && outputPath.endsWith('.html')) {
-      // We remove outer brackets from links
-      const output = content.replace(/(\[+(\<a(.*?)\<\/a\>)\]+)/g, '$2');
-      return output;
-    }
-    return content;
-  });
+	/* Foambubble wiki links */
+	evc.addTransform('wiki-links', (content, outputPath) => {
+		if (outputPath && outputPath.endsWith('.html')) {
+			// We remove outer brackets from links
+			const output = content.replace(/(\[+(\<a(.*?)\<\/a\>)\]+)/g, '$2');
+			return output;
+		}
+		return content;
+	});
 
-  evc.addNunjucksAsyncShortcode('image', imageShortcode);
+	evc.addNunjucksAsyncShortcode('image', imageShortcode);
 
-  // Return the smallest number argument
-  evc.addFilter('min', (...numbers) => {
-    return Math.min.apply(null, numbers);
-  });
+	// Return the smallest number argument
+	evc.addFilter('min', (...numbers) => {
+		return Math.min.apply(null, numbers);
+	});
 
-  /* Filter for dates with luxon */
-  evc.addFilter('asPostDate', dateObj =>
-    DT.fromJSDate(dateObj).toLocaleString(DT.DATE_MED),
-  );
+	/* Filter for dates with luxon */
+	evc.addFilter('asPostDate', dateObj =>
+		DT.fromJSDate(dateObj).toLocaleString(DT.DATE_MED),
+	);
 
-  evc.addFilter('readableDate', dateObj => {
-    return DT.fromJSDate(dateObj, { zone: 'gmt' }).toFormat('dd LLL yyyy');
-  });
+	evc.addFilter('readableDate', dateObj => {
+		return DT.fromJSDate(dateObj, {zone: 'gmt'}).toFormat('dd LLL yyyy');
+	});
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  evc.addFilter('htmlDateString', dateObj => {
-    return DT.fromJSDate(dateObj, { zone: 'gmt' }).toFormat('yyyy-LL-dd');
-  });
+	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+	evc.addFilter('htmlDateString', dateObj => {
+		return DT.fromJSDate(dateObj, {zone: 'gmt'}).toFormat('yyyy-LL-dd');
+	});
 
-  // Get the first `n` elements of a collection.
-  evc.addFilter('head', (array, n) => {
-    if (!Array.isArray(array) || array.length === 0) {
-      return [];
-    }
-    if (n < 0) {
-      return array.slice(n);
-    }
+	// Get the first `n` elements of a collection.
+	evc.addFilter('head', (array, n) => {
+		if (!Array.isArray(array) || array.length === 0) {
+			return [];
+		}
+		if (n < 0) {
+			return array.slice(n);
+		}
 
-    return array.slice(0, n);
-  });
+		return array.slice(0, n);
+	});
 
-  const filterTagList = tags => {
-    return (tags || []).filter(
-      tag => ['all', 'nav', 'pages', 'post', 'posts'].indexOf(tag) === -1,
-    );
-  };
-  evc.addFilter('filterTagList', filterTagList);
+	const filterTagList = tags => {
+		return (tags || []).filter(
+			tag => ['all', 'nav', 'pages', 'post', 'posts'].indexOf(tag) === -1,
+		);
+	};
+	evc.addFilter('filterTagList', filterTagList);
 
-  /* Create an array of all these tags */
-  evc.addCollection('tagList', collection => {
-    let tagSet = new Set();
-    collection.getAll().forEach(item => {
-      (item.data.tags || []).forEach(tag => tagSet.add(tag));
-    });
+	/* Create an array of all these tags */
+	evc.addCollection('tagList', collection => {
+		let tagSet = new Set();
+		collection.getAll().forEach(item => {
+			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		});
 
-    return filterTagList([...tagSet]);
-  });
+		return filterTagList([...tagSet]);
+	});
 
-  const markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: true,
-    replaceLink: (link, env) => {
-      const isRelativePattern = /^(?!http|\/).*/;
-      const lastSegmentPattern = /[^\/]+(?=\/$|$)/i;
-      const isRelative = isRelativePattern.test(link);
+	const markdownLibrary = markdownIt({
+		html: true,
+		breaks: true,
+		linkify: true,
+		typographer: true,
+		replaceLink: (link, env) => {
+			const isRelativePattern = /^(?!http|\/).*/;
+			const lastSegmentPattern = /[^\/]+(?=\/$|$)/i;
+			const isRelative = isRelativePattern.test(link);
 
-      if (isRelative) {
-        const hasLastSegment = lastSegmentPattern.exec(env.page.url);
-        // If it's nested, replace the last segment
-        if (hasLastSegment && env.page.url) {
-          return env.page.url.replace(lastSegmentPattern, link);
-        }
-        // If it's at root, just add the beginning slash
-        return env.page.url + link;
-      }
+			if (isRelative) {
+				const hasLastSegment = lastSegmentPattern.exec(env.page.url);
+				// If it's nested, replace the last segment
+				if (hasLastSegment && env.page.url) {
+					return env.page.url.replace(lastSegmentPattern, link);
+				}
+				// If it's at root, just add the beginning slash
+				return env.page.url + link;
+			}
 
-      return link;
-    },
-  })
-    .use(markdownItAnchor, {
-      permalink: markdownItAnchor.permalink.ariaHidden({
-        placement: 'after',
-        class: 'direct-link',
-        symbol: '#',
-        level: [1, 2, 3, 4],
-      }),
-      slugify: evc.getFilter('slug'),
-    })
-    .use(markdownItEmoji) // Our deer emojis **must** work!
-    .use(markdownItReplaceLink);
+			return link;
+		},
+	})
+		.use(markdownItAnchor, {
+			permalink: markdownItAnchor.permalink.ariaHidden({
+				placement: 'after',
+				class: 'direct-link',
+				symbol: '#',
+				level: [1, 2, 3, 4],
+			}),
+			slugify: evc.getFilter('slug'),
+		})
+		.use(markdownItEmoji) // Our deer emojis **must** work!
+		.use(markdownItReplaceLink)
+		.use(markdownItMath);
 
-  evc.setLibrary('md', markdownLibrary);
-  evc.addFilter('markdown', content => markdownLibrary.render(content));
-  evc.addPairedShortcode('markdown', content => md.render(content));
+	evc.setLibrary('md', markdownLibrary);
+	evc.addFilter('markdown', content => markdownLibrary.render(content));
+	evc.addPairedShortcode('markdown', content => md.render(content));
 
-  // Override Browsersync defaults (used only with --serve)
-  evc.setBrowserSyncConfig({
-    ui: false,
-    ghostMode: false,
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+	// Override Browsersync defaults (used only with --serve)
+	evc.setBrowserSyncConfig({
+		ui: false,
+		ghostMode: false,
+		callbacks: {
+			ready: (err, browserSync) => {
+				const content_404 = fs.readFileSync('_site/404.html');
 
-        browserSync.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' });
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-  });
+				browserSync.addMiddleware('*', (req, res) => {
+					// Provides the 404 content without redirect.
+					res.writeHead(404, {'Content-Type': 'text/html; charset=UTF-8'});
+					res.write(content_404);
+					res.end();
+				});
+			},
+		},
+	});
 
-  /* And now our passthru copies! */
-  evc.addPassthroughCopy('static/content');
-  evc.addPassthroughCopy('static/img');
-  evc.addPassthroughCopy('static/fonts');
-  evc.addPassthroughCopy('static/icons');
-  evc.addPassthroughCopy('static/media');
-  evc.addPassthroughCopy('.well-known');
-  evc.addPassthroughCopy({ 'stylus/vendor/css': 'css/' });
+	/* And now our passthru copies! */
+	evc.addPassthroughCopy('static/content');
+	evc.addPassthroughCopy('static/img');
+	evc.addPassthroughCopy('static/fonts');
+	evc.addPassthroughCopy('static/icons');
+	evc.addPassthroughCopy('static/media');
+	evc.addPassthroughCopy('.well-known');
+	evc.addPassthroughCopy({'stylus/vendor/css': 'css/'});
 
-  return {
-    markdownTemplateEngine: 'njk',
-    pathPrefix: '/',
-    dir: {
-      input: '.',
-      includes: '_includes',
-      data: '_data',
-      output: '_site',
-    },
-  };
+	return {
+		markdownTemplateEngine: 'njk',
+		pathPrefix: '/',
+		dir: {
+			input: '.',
+			includes: '_includes',
+			data: '_data',
+			output: '_site',
+		},
+	};
 };
--- posts/Culture-Sans-Frontiéres.md
+++ posts/Culture-Sans-Frontiéres.md
@@ -1,8 +1,22 @@
 ---
-title: Culture Sans Frontiéres - Homogenization of Peoples and the New Age
-description: How we lost our own war on our identity, and why we'll never get it back.
+title: Witchcraft of the digital age
+description: Manipulating the sphere of the public unconscious, and how to twist the information dystopia to your will.
 tags: ['writeup', 'sociology']
 ---
 
-You may have come across a point in your being online "journey" (so-to-speak),
-in where you will have encountered a person who seems to have existed in a completely dissimilar vein to yourself; they may be from another country & you would have wondered to ask them, "how did you get so good at English?"
+![Alt Image](/static/img/surrealcity.jpg)
+###### The distorted lens of "reality."
+
+||||||||||| `I stared at the echo in front of me`
+
+||||||||||| `I stared at the echo in front of me`
+
+## Merging the souls of the wired
+
+The profound influence the [[wired]](/terms/wired) has had upon the [[collective unconscious]](/terms/collective_unconscious), by proxy has also homogenized the cultural landscape, primarily in what we consider to be the "western" sphere.
+
+"Truth" has altered the way in which it is imparted into us, rather than eradicated. In essence, truth's effect is more akin to a shared delusion; as opposed to an idea that has been decided by those with more societal authority than us. Whereas rife skepticism within public sphere is now commonplace, this begs the question of who will be the one who decides to share their truth to appeal to, the now much less receptive mind?
+
+## Lay the seed
+
+The inferior souls among people are easily swayed by ideas decorated in the terms of the familiar, unbeknownst to the necessity of its remoteness to its actual predicated belief. In the minds of such people, the groundwork is far easily laid once a framework of understanding is identified.
